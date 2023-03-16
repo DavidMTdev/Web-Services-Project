05557728d6cb686e6316d5403b562f600cc6c0c9
DavidMTdev <david.teixeira31@gmail.com>
2023-03-16 13:26:49 +0100

    add new routes method

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const { LOADIPHLPAPI } = require('dns')
 const http = require('http')
 
 const host = 'localhost'
@@ -5,173 +6,283 @@ const port = 3000
 
 const databases = {}
 
-const hendlerRequest = (req, res) => {
-    console.log(`method: ${req.method} host: ${req.headers.host} url: ${req.url}`)
+const matchURL = (path, url) => {
+    const array = path.split(/:(\w+)/)
 
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Request-Method', '*')
-    res.setHeader('Access-Control-Allow-Headers', '*')
+    let a = '^'
+    array.forEach(row => {
+        if (row === '/') {
+            a += '\\/'
+        } else if (row === '') {
+            a += ''
+        } else {
+            a += `(?<${row}>\\w+)`
+        }
+    })
+    a += '$'
 
-    const url = new URL(`http://${req.headers.host}${req.url}`)
-    console.log(url)
+    const regexp = new RegExp(a)
+    const pathname = url.pathname.match(regexp)
 
-    // const pathname = url.pathname.match(/^\/$/)
-    // console.log(pathname)
-
-    // const pathname2 = url.pathname.match(/^\/(?<database>\w+)$/)
-    // if (pathname2) {
-    //     const database = pathname2.groups.database
-    //     console.log(database)
-
-    //     const d = pathname2.input.replace(`/${database}`, '/{database}')
-    //     console.log(d)
-    // }
-
-    // const r = url.pathname.replace('/cinema', '/{database}')
-
-    // if (r = '/{database}') {
-    //     console.log(r)
-    // }    
-
-    const matchURL = (path) => {
-        const array = path.split(/{(\w+)}/)
-
-        let a = '^'
-        array.forEach(row => {
-            if (row === '/') {
-                a += '\\/'
-            } else if (row === '') {
-                a += ''
-            } else {
-                a += `(?<${row}>\\w+)`
-            }
-        })
-        a += '$'
-
-        const regexp = new RegExp(a)
-        const pathname = url.pathname.match(regexp)
-
-        return pathname
-    }
+    return pathname
+}
 
 
-    const route = (method, path, handler) => {
+// const route = (method, path, handler) => {
+//     const pathname = matchURL(path)
+//     console.log(pathname)
+//     if (req.method === method) {
+//         if (pathname) {
+//             handler()
+//         }
+//     } else {
+//         console.log('Method not found')
+//     }
+// }
+
+const rootRouter = {
+    use: (path, router) => {
         const pathname = matchURL(path)
 
-        if (req.method === method) {
-            if (pathname) {
-                handler()
-            }
-        } else {
-            console.log('Method not found')
+        if (pathname) {
+            console.log(pathname.groups)
         }
+    },
+    get: (path, handler) => {
+        route('GET', path, handler)
+    },
+    post: (path, handler) => {
+        route('POST', path, handler)
+    },
+    put: (path, handler) => {
+        route('PUT', path, handler)
+    },
+    delete: (path, handler) => {
+        route('DELETE', path, handler)
+    },
+    options: (path, handler) => {
+        route('OPTIONS', path, handler)
+    }
+}
+
+const serverHttp = () => {
+    // const server = http.createServer()
+    const server = http.createServer((req, res) => {
+        handlerRequest(req, res)
+    })
+
+    let route = {
+        GET: {},
+        POST: {},
+        PUT: {},
+        DELETE: {},
+        OPTIONS: {}
     }
 
-    const rootRouter = {
-        use: (path, router) => {
-            const pathname = matchURL(path)
+    const handlerRequest = async (req, res) => {
+        console.log(`method: ${req.method} host: ${req.headers.host} url: ${req.url}`)
+        const url = new URL(`http://${req.headers.host}${req.url}`)
+        const response = {
+            status: 200,
+            message: 'OK',
+            data: null,
+            error: null
+        }
+
+        for (const path in route[req.method]) {
+            const pathname = matchURL(path, url)
+            console.log(pathname)
 
             if (pathname) {
                 console.log(pathname.groups)
+                const handler = route[req.method][path]
+       
+                if (handler) {
+                    await handler(req, res, response)
+                } else {
+                    response.status = 404
+                    response.message = 'Not found'
+                }
             }
-        },
-        get: (path, handler) => {
-            route('GET', path, handler)
-        },
-        post: (path, handler) => {
-            route('POST', path, handler)
-        },
-        put: (path, handler) => {
-            route('PUT', path, handler)
-        },
-        delete: (path, handler) => {
-            route('DELETE', path, handler)
-        },
-        options: (path, handler) => {
-            route('OPTIONS', path, handler)
+            
+        }
+
+        res.setHeader('Content-Type', 'application/json')
+        res.writeHead(response.status)
+        res.end(JSON.stringify(response.message))
+
+        // if (response.error) {
+        //     res.end(JSON.stringify({
+        //         message: response.message,
+        //         error: response.error
+        //     }))
+        // } else {
+        //     res.end(JSON.stringify({
+        //         message: response.message,
+        //         data: response.data
+        //     }))
+        // }
+    }
+
+    const use = () => {
+        console.log('Use')
+    }
+
+    const get = (path, handler) => {
+
+        if (!route['GET']) {
+            route['GET'] = {}
+        }
+
+        if (!route['GET'][path]) {
+            route['GET'][path] = handler
         }
     }
 
-    // rootRouter.use('/', route)
-    rootRouter.get('/', (req, res) => {
-        console.log('Hello')
-    })
+    const post = (path, handler) => {
+        if (!route['POST']) {
+            route['POST'] = {}
+        }
 
-    // route('GET', '/')
-    // route('GET', '/{database}')
-    // route('GET', '/{database}/{table}')
+        if (!route['POST'][path]) {
+            route['POST'][path] = handler
+        }
+    }
 
+    const put = (path, handler) => {
+        if (!route['PUT']) {
+            route['PUT'] = {}
+        }
 
+        if (!route['PUT'][path]) {
+            route['PUT'][path] = handler
+        }
+    }
 
-    
-    // const pathname3 = url.pathname.match(/^\/(?<database>\w+)\/(?<table>\w+)$/)
-    // console.log(pathname3)
+    const deleted = (path, handler) => {
+        if (!route['DELETE']) {
+            route['DELETE'] = {}
+        }
 
-    res.end('Hello')
+        if (!route['DELETE'][path]) {
+            route['DELETE'][path] = handler
+        }
+    }
 
-    // route.use('/{database}', apiRouter)
-  
+    const options = (path, handler) => {
+        if (!route['OPTIONS']) {
+            route['OPTIONS'] = {}
+        }
 
-    // if (req.method === 'OPTIONS') {
-    //     const response = {
-    //         description: 'List of methods',
-    //         methods: ['GET', 'POST', 'PUT', 'DELETE']
-    //     }
+        if (!route['OPTIONS'][path]) {
+            route['OPTIONS'][path] = handler
+        }
+    }
 
-    //     res.writeHead(200)
-    //     res.end(JSON.stringify(response))
+    const listen = (port, host, callback) => {
+        server.listen(port, host, callback)
+    }
 
-    // } else if (req.method === 'GET') {
-    //     const response = {}
-
-    //     res.writeHead(200)
-    //     res.end(JSON.stringify(response))
-
-    // } else if (req.method === 'POST') {
-    //     let body = ''
-    //     req.on('data', chunk => {
-    //         body += chunk.toString() // convert Buffer to string
-    //     })
-    //     req.on('end', () => {
-    //         console.log(body)
-
-    //         const response = {}
-
-    //         res.writeHead(201)
-    //         res.end(JSON.stringify(response)) 
-    //     })
-    // } else if (req.method === 'PUT') {
-    //     let body = ''
-    //     req.on('data', chunk => {
-    //         body += chunk.toString() // convert Buffer to string
-    //     })
-    //     req.on('end', () => {
-    //         console.log(body)
-
-    //         const response = {}
-
-    //         res.writeHead(200)
-    //         res.end(JSON.stringify(response)) 
-    //     })
-    // } else if (req.method === 'DELETE') {
-    //     const response = {}
-
-    //     res.writeHead(200)
-    //     res.end(JSON.stringify(response)) 
-
-    // } else {
-    //     const error = {
-    //         message: 'Method not allowed'
-    //     }
-
-    //     res.writeHead(405)
-    //     res.end(JSON.stringify(error))
-    // }
+    return {
+        use,
+        get,
+        post,
+        put,
+        delete: deleted,
+        options,
+        listen
+    }
 }
 
-const server = http.createServer(hendlerRequest)
+// const hendlerRequest = (req, res) => {
+//     console.log(`method: ${req.method} host: ${req.headers.host} url: ${req.url}`)
 
-server.listen(port, host, () => {
+//     res.setHeader('Content-Type', 'application/json')
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader('Access-Control-Request-Method', '*')
+//     res.setHeader('Access-Control-Allow-Headers', '*')
+
+//     const url = new URL(`http://${req.headers.host}${req.url}`)
+//     console.log(url)
+
+//     // rootRouter.use('/', route)
+//     rootRouter.get('/', (req, res) => {
+//         console.log('Hello')
+//     })
+
+//     const get_database = (database) => {
+//         console.log('Database')  
+//     }
+
+
+//     rootRouter.get('/:database', (req, res) => {
+//         console.log(`Database: `)
+//     })
+
+//     // route.use('/{database}', apiRouter)
+// }
+
+const app = serverHttp()
+
+// app.use()
+app.options('/', (req, res, response) => {
+    console.log('options')
+    response.status = 200
+    response.message = {
+        description: 'List of methods',
+        methods: ['GET', 'POST']
+    }
+})
+
+app.get('/', (req, res, response) => {
+    console.log('root')
+    response.status = 200
+    response.message = {
+        description: 'List of database',
+        databases: Object.keys(databases)
+    }
+})
+
+app.post('/', async (req, res, response) => {
+    let body = ''
+    req.on('data', chunk => {
+        body += chunk.toString() // convert Buffer to string
+    })
+
+    await req.on('end', () => {
+        const obj = JSON.parse(body)
+
+        if (obj.database) {
+            databases[obj.database] = obj.database
+
+            response.status = 200
+            response.message = {
+                description: 'Database created',
+                database: obj.database
+            }
+        } else {
+
+            // response.error = true
+            response.status = 400
+            response.message = {
+                message: 'Database name is required'
+            }
+        }
+    })
+
+    req.on('error', (err) => {
+        console.error(err)
+    })
+})
+
+app.get('/:database', (req, res, response) => {
+    response.status = 200
+    response.message = {
+        description: 'List of tables',
+        table: []
+    }
+})
+
+app.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`)
 })
