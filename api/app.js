7da6306e0f129062b1a0ada629d751dbefa37bff
DavidMTdev <david.teixeira31@gmail.com>
2023-03-17 12:15:30 +0100

    update app

--- api/app.js
+++ api/app.js
@@ -1,29 +1,11 @@
 const ServerHttp = require('./lib/server')
-
-const rootRouter = require('../api/lib/routes/index')
-
-console.log(rootRouter.getRoutes());
+const rootRouter = require('./routes/index')
+const databaseRouter = require('./routes/database')
 
 const app = new ServerHttp()
 
 app.route('/', rootRouter)
+app.route('/:databse', databaseRouter)
 
-// app.router.options('/', (req, res) => {
-//     console.log('options root')
-// })
-
-// app.router.get('/', (req, res) => {
-//     console.log('get root')
-// })
-
-// app.router.post('/', (req, res) => {
-//     console.log('post root')
-// })
-
-// app.router.get('/:database', (req, res) => {
-//     console.log('get database')
-// })
-
-console.log(app.router.getRoutes())
-
+// console.log(app.router.getRoutes())
 app.listen()
