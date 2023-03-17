fec4d8513745475f908f879e6a5a60f8d6061943
DavidMTdev <david.teixeira31@gmail.com>
2023-03-17 15:07:17 +0100

    prepare dbManager

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 const ServerHttp = require('./lib/server')
+// const DatabaseManager = require('./db')
 const rootRouter = require('./routes/index')
 const databaseRouter = require('./routes/database')
 
+// const dbManager = new DatabaseManager()
 const app = new ServerHttp()
 
 app.route('/', rootRouter)
