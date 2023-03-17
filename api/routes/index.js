1557ff3d9b49d47ed222a3c22d7e7d6142dafaa6
DavidMTdev <david.teixeira31@gmail.com>
2023-03-17 12:17:05 +0100

    update route root

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,54 @@
-const Router = require('../router')
+const Router = require('../lib/router')
+const { Database, databases } = require('../db')
 
 const router = new Router()
 
 router.options('/', (req, res) => {
-    console.log('options root')
+    console.log(`${req.method} root`)
+
+    const response = {
+        description: 'List of methods',
+        methods: ['GET', 'POST']
+    }
+
+    res.status = 200
+    res.message = response
 })
 
 router.get('/', (req, res) => {
-    console.log('get root')
+    console.log(`${req.method} root`)
+
+    const response = {
+        description: 'List of database',
+        databases: databases()
+    }
+
+    res.status = 200
+    res.message = response
 })
 
-router.post('/', (req, res) => {
-    console.log('post root')
+router.post('/', async (req, res) => {
+    console.log(`${req.method} root`)
+    console.log(req.body)
+
+    if (req.body.database) {
+        const response = {
+            description: 'Database created',
+            database: req.body.database
+        }
+
+        const db = new Database(req.body.database)
+
+        res.status = 201
+        res.message = response
+    } else {
+        const error = {
+            message: 'Database name is required'
+        }
+        
+        res.status = 400
+        res.message = error
+    }
 })
 
 module.exports = router
