7dcf2e3e21b0f334193c11baf3e7696a42d56fb8
DavidMTdev <david.teixeira31@gmail.com>
2023-03-17 15:06:12 +0100

    update route root

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,8 +4,6 @@ const { Database, databases } = require('../db')
 const router = new Router()
 
 router.options('/', (req, res) => {
-    console.log(`${req.method} root`)
-
     const response = {
         description: 'List of methods',
         methods: ['GET', 'POST']
@@ -16,11 +14,9 @@ router.options('/', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-    console.log(`${req.method} root`)
-
     const response = {
         description: 'List of database',
-        databases: databases()
+        databases: Object.keys(databases())
     }
 
     res.status = 200
@@ -28,27 +24,25 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    console.log(`${req.method} root`)
-    console.log(req.body)
-
-    if (req.body.database) {
-        const response = {
-            description: 'Database created',
-            database: req.body.database
-        }
-
-        const db = new Database(req.body.database)
-
-        res.status = 201
-        res.message = response
-    } else {
+    if (!req.body.database) {
         const error = {
             message: 'Database name is required'
         }
         
         res.status = 400
         res.message = error
+        return
     }
+
+    const response = {
+        description: 'Database created',
+        database: req.body.database
+    }
+
+    const db = new Database(req.body.database)
+
+    res.status = 201
+    res.message = response
 })
 
 module.exports = router
