4540b6e4180811fa8d2158adefe3ff1b2ee2c71d
DavidMTdev <david.teixeira31@gmail.com>
2023-03-23 17:14:51 +0100

    Add route Column

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ const ServerHttp = require('./lib/server')
 const rootRouter = require('./routes/index')
 const databaseRouter = require('./routes/database')
 const tableRouter = require('./routes/table')
+const columnsRouter = require('./routes/columns')
+const columnRouter = require('./routes/column')
 
 // const dbManager = new DatabaseManager()
 const app = new ServerHttp()
@@ -10,6 +12,9 @@ const app = new ServerHttp()
 app.route('/', rootRouter)
 app.route('/:databse', databaseRouter)
 app.route('/:database/:table', tableRouter)
+app.route('/:database/:table/columns', columnsRouter)
+app.route('/:database/:table/columns/:column', columnRouter)
+
 
 // console.log(app.router.getRoutes())
 app.listen()
