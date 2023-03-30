ed7d3eab71af4ec8188d0f8abdde04433133ee6e
DavidMTdev <david.teixeira31@gmail.com>
2023-03-30 12:34:38 +0200

    init drawer

---
+++ app/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { useTheme } from '@mui/material/styles'
+
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+
+import AppBar from './components/AppBar'
+import AppDrawer from './components/AppDrawer'
+import AppMain from './components/AppMain'
+
+export default function App() {
+  const theme = useTheme();
+  const [open, setOpen] = useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar theme={theme} open={open} click={handleDrawerOpen} />
+      <AppDrawer theme={theme} open={open} click={handleDrawerClose} />
+      <AppMain theme={theme} open={open} />
+      {/* <Main open={open}>
+        <DrawerHeader />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+          sapien faucibus et molestie ac.
+        </Typography>
+      </Main> */}
+    </Box>
+  );
+}
