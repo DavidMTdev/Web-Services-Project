5c7570625c7f516b917610150a6e04d0194e1743
DavidMTdev <david.teixeira31@gmail.com>
2023-04-02 18:46:23 +0200

    update style AppBar and drawer

--- app/src/components/AppBar.jsx
+++ app/src/components/AppBar.jsx
@@ -2,28 +2,42 @@ import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import MenuIcon from '@mui/icons-material/Menu'
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 
 import AppBarStyled from './styles/AppBar'
 
 const AppBar = ({ theme, open, click }) => {
-    return ( 
-        <AppBarStyled position="fixed" open={open}>
-            <Toolbar>
-            <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={click}
-                edge="start"
-                sx={{ mr: 2, ...(open && { display: 'none' }) }}
-            >
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap component="div">
-                Persistent drawer
-            </Typography>
-            </Toolbar>
-        </AppBarStyled> 
-    )
+  const iconBackColor = 'grey.100'
+  const iconBackColorOpen = 'grey.200'
+
+  return ( 
+    <AppBarStyled position="fixed" open={open}>
+      <Toolbar>
+        {/* <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={click}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+        >
+          <MenuIcon />
+        </IconButton> */}
+        <IconButton
+          disableRipple
+          aria-label="open drawer"
+          onClick={click}
+          edge="start"
+          color="inherit"
+          sx={{ mr: 2 }}
+        >
+          {open ? <MenuOpenIcon /> : <MenuIcon />}
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          InMemory My Admin
+        </Typography>
+      </Toolbar>
+    </AppBarStyled> 
+  )
 }
  
 export default AppBar
